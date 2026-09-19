22e52c55aac729cefb438a580ae70904ad7d0125
Leo <i@hardrain980.com>
2026-09-19 16:26:41 +0800

    unit test updates
    Signed-off-by: Leo <i@hardrain980.com>

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,27 +1,41 @@
+import asyncio
 import grpc
+import pytest
+
 from fsh.wrapper import Path
 from base import FSHTestBase
 
+
+@pytest.mark.asyncio
 class TestPath(FSHTestBase):
-	def test_isdir(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert Path(c).isdir(f'{self.PATH}/directory') is True
-			assert Path(c).isdir(f'{self.PATH}/file') is False
-			assert Path(c).isdir(f'{self.PATH}/link') is False
+	async def test_isdir(self):
+		def test():
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert Path(c).isdir(f'{self.PATH}/directory') is True
+				assert Path(c).isdir(f'{self.PATH}/file') is False
+				assert Path(c).isdir(f'{self.PATH}/link') is False
+		await asyncio.to_thread(test)
+			
 
-	def test_isfile(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert Path(c).isfile(f'{self.PATH}/directory') is False
-			assert Path(c).isfile(f'{self.PATH}/file') is True
-			assert Path(c).isfile(f'{self.PATH}/link') is True
+	async def test_isfile(self):
+		def test():
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert Path(c).isfile(f'{self.PATH}/directory') is False
+				assert Path(c).isfile(f'{self.PATH}/file') is True
+				assert Path(c).isfile(f'{self.PATH}/link') is True
+		await asyncio.to_thread(test)
 
-	def test_islink(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert Path(c).islink(f'{self.PATH}/directory') is False
-			assert Path(c).islink(f'{self.PATH}/file') is False
-			assert Path(c).islink(f'{self.PATH}/link') is True
+	async def test_islink(self):
+		def test():
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert Path(c).islink(f'{self.PATH}/directory') is False
+				assert Path(c).islink(f'{self.PATH}/file') is False
+				assert Path(c).islink(f'{self.PATH}/link') is True
+		await asyncio.to_thread(test)
 
-	def test_exists(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert Path(c).exists(f'{self.PATH}/file') is True
-			assert Path(c).exists(f'{self.PATH}/not-exist') is False
+	async def test_exists(self):
+		def test():
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert Path(c).exists(f'{self.PATH}/file') is True
+				assert Path(c).exists(f'{self.PATH}/not-exist') is False
+		await asyncio.to_thread(test)
--- tests/test_syscall.py
+++ tests/test_syscall.py
@@ -1,148 +1,125 @@
+import asyncio
 import os
 import grpc
 import pytest
+
 from fsh.wrapper import Syscall
 from base import FSHTestBase
 
+
+@pytest.mark.asyncio
 class TestSyscall(FSHTestBase):
-	def test_read_lseek(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			fd = Syscall(c).open(f'{self.PATH}/file', os.O_RDONLY, 0o0666)
-			buf = Syscall(c).read(fd, 4)
-			assert buf == b'fsh-'
-			Syscall(c).lseek(fd, 9, os.SEEK_SET)
-			buf = Syscall(c).read(fd, 4096)
-			assert buf == b'123\n'
+	async def test_read_lseek(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				fd = Syscall(c, d).open(f'{self.PATH}/file', os.O_RDONLY, 0o0666)
+				buf = os.read(fd, 4)
+				assert buf == b'fsh-'
+				os.lseek(fd, 9, os.SEEK_SET)
+				buf = os.read(fd, 4096)
+				assert buf == b'123\n'
+		await asyncio.to_thread(test)
 
-	def test_write_lseek(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			fd = Syscall(c).open(f'{self.PATH}/file', os.O_WRONLY|os.O_CREAT|os.O_TRUNC, 0o0666)
-			assert Syscall(c).write(fd, b'test123') == 7
-			assert Syscall(c).lseek(fd, 5, os.SEEK_CUR) == 12
-			assert Syscall(c).write(fd, b'test456') == 7
-			Syscall(c).close(fd)
+	async def test_write_lseek(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				fd = Syscall(c, d).open(f'{self.PATH}/file', os.O_WRONLY|os.O_CREAT|os.O_TRUNC, 0o0666)
+				assert os.write(fd, b'test123') == 7
+				assert os.lseek(fd, 5, os.SEEK_CUR) == 12
+				assert os.write(fd, b'test456') == 7
+				os.close(fd)
 
-			with open(f'{self.PATH}/file', 'rb') as f:
-				assert f.read() == b'test123\0\0\0\0\0test456'
+				with open(f'{self.PATH}/file', 'rb') as f:
+					assert f.read() == b'test123\0\0\0\0\0test456'
+		await asyncio.to_thread(test)
 
-	def test_invalid_open(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			with pytest.raises(FileNotFoundError):
-				Syscall(c).open(f'{self.PATH}/not-exist', os.O_RDONLY, 0o0666)
+	async def test_invalid_open(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				with pytest.raises(FileNotFoundError):
+					Syscall(c, d).open(f'{self.PATH}/not-exist', os.O_RDONLY, 0o0666)
+		await asyncio.to_thread(test)
 
-	def test_invalid_close(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			with pytest.raises(OSError):
-				Syscall(c).close(50000) # not existing fd
+	async def test_unlink(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert os.path.exists(f'{self.PATH}/file') is True
+				Syscall(c, d).unlink(f'{self.PATH}/file')
+				assert os.path.exists(f'{self.PATH}/file') is False
 
-	def test_invalid_read(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			with pytest.raises(OSError):
-				Syscall(c).read(50000, 1) # not existing fd
+				with pytest.raises(OSError):
+					Syscall(c, d).unlink(f'{self.PATH}/not-exist')
+		await asyncio.to_thread(test)
 
-	def test_invalid_write(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			with pytest.raises(OSError):
-				fd = Syscall(c).open(f'{self.PATH}/file', os.O_RDONLY, 0o0666)
-				Syscall(c).write(fd, b'garbage') # read-only fd
+	async def test_mkdir_rmdir(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				assert os.path.exists(f'{self.PATH}/new-directory') is False
+				Syscall(c, d).mkdir(f'{self.PATH}/new-directory', 0o755)
+				assert os.path.exists(f'{self.PATH}/new-directory') is True
 
-	def test_invalid_lseek(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			with pytest.raises(OSError):
-				Syscall(c).lseek(50000, 0, os.SEEK_CUR) # not existing fd
+				assert os.path.exists(f'{self.PATH}/directory') is True
+				Syscall(c, d).rmdir(f'{self.PATH}/directory')
+				assert os.path.exists(f'{self.PATH}/directory') is False
 
-	def test_fsync(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			fd = Syscall(c).open(f'{self.PATH}/file', os.O_WRONLY|os.O_CREAT|os.O_TRUNC, 0o0666)
-			Syscall(c).write(fd, b'test123')
-			Syscall(c).fsync(fd)
-			Syscall(c).close(fd)
+				with pytest.raises(OSError):
+					Syscall(c, d).rmdir(f'{self.PATH}/not-exist')
 
-			with open(f'{self.PATH}/file', 'rb') as f:
-				assert f.read() == b'test123'
+				with pytest.raises(OSError):
+					Syscall(c, d).mkdir(f'{self.PATH}/file', 0o755)
+		await asyncio.to_thread(test)
 
-			with pytest.raises(OSError):
-				Syscall(c).fsync(50000) # not existing fd
+	async def test_listdir(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				dirs = Syscall(c, d).listdir(self.PATH)
+				dirs_golden = os.listdir(self.PATH)
+				assert set(dirs) == set(dirs_golden)
 
-	def test_unlink(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert os.path.exists(f'{self.PATH}/file') is True
-			Syscall(c).unlink(f'{self.PATH}/file')
-			assert os.path.exists(f'{self.PATH}/file') is False
+				with pytest.raises(OSError):
+					Syscall(c, d).listdir(f'{self.PATH}/not-exist')
+		await asyncio.to_thread(test)
 
-			with pytest.raises(OSError):
-				Syscall(c).unlink(f'{self.PATH}/not-exist')
+	async def test_utime(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				Syscall(c, d).utime(f'{self.PATH}/file', 10, 20)
+				stat = os.stat(f'{self.PATH}/file')
+				assert stat.st_atime == 10
+				assert stat.st_mtime == 20
 
-	def test_truncate(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			fd = Syscall(c).open(f'{self.PATH}/file', os.O_WRONLY|os.O_CREAT|os.O_TRUNC, 0o0666)
-			Syscall(c).write(fd, b'test123')
-			Syscall(c).fsync(fd)
+				with pytest.raises(OSError):
+					Syscall(c, d).utime(f'{self.PATH}/not-exist', 10, 20)
+		await asyncio.to_thread(test)
 
-			with open(f'{self.PATH}/file', 'rb') as f:
-				assert f.read() == b'test123'
+	async def test_stat_path(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				stat = Syscall(c, d).stat(f'{self.PATH}/file')
+				stat_golden = os.stat(f'{self.PATH}/file')
+				assert stat == stat_golden
 
-			Syscall(c).truncate(fd, 4)
-			Syscall(c).fsync(fd)
+				with pytest.raises(OSError):
+					Syscall(c, d).stat(f'{self.PATH}/not-exist')
+		await asyncio.to_thread(test)
 
-			with open(f'{self.PATH}/file', 'rb') as f:
-				assert f.read() == b'test'
+	async def test_stat_fd(self):
+		def test():
+			d = f'{self.PREFIX}.fd.sock'
+			with grpc.insecure_channel(f'unix://{self.PREFIX}.sock') as c:
+				fd = Syscall(c, d).open(f'{self.PATH}/file', os.O_RDONLY, 0o0644)
+				stat = Syscall(c, d).stat(fd)
+				stat_golden = os.stat(f'{self.PATH}/file')
+				assert stat == stat_golden
 
-			Syscall(c).close(fd)
-
-			with pytest.raises(OSError):
-				Syscall(c).truncate(50000, 4) # not existing fd
-
-	def test_mkdir_rmdir(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			assert os.path.exists(f'{self.PATH}/new-directory') is False
-			Syscall(c).mkdir(f'{self.PATH}/new-directory', 0o755)
-			assert os.path.exists(f'{self.PATH}/new-directory') is True
-
-			assert os.path.exists(f'{self.PATH}/directory') is True
-			Syscall(c).rmdir(f'{self.PATH}/directory')
-			assert os.path.exists(f'{self.PATH}/directory') is False
-
-			with pytest.raises(OSError):
-				Syscall(c).rmdir(f'{self.PATH}/not-exist')
-
-			with pytest.raises(OSError):
-				Syscall(c).mkdir(f'{self.PATH}/file', 0o755)
-
-	def test_listdir(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			dirs = Syscall(c).listdir(self.PATH)
-			dirs_golden = os.listdir(self.PATH)
-			assert set(dirs) == set(dirs_golden)
-
-			with pytest.raises(OSError):
-				Syscall(c).listdir(f'{self.PATH}/not-exist')
-
-	def test_utime(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			Syscall(c).utime(f'{self.PATH}/file', 10, 20)
-			stat = os.stat(f'{self.PATH}/file')
-			assert stat.st_atime == 10
-			assert stat.st_mtime == 20
-
-			with pytest.raises(OSError):
-				Syscall(c).utime(f'{self.PATH}/not-exist', 10, 20)
-
-	def test_stat_path(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			stat = Syscall(c).stat(f'{self.PATH}/file')
-			stat_golden = os.stat(f'{self.PATH}/file')
-			assert stat == stat_golden
-
-			with pytest.raises(OSError):
-				Syscall(c).stat(f'{self.PATH}/not-exist')
-
-	def test_stat_fd(self):
-		with grpc.insecure_channel(f'127.0.0.1:{self.PORT}') as c:
-			fd = Syscall(c).open(f'{self.PATH}/file', os.O_RDONLY, 0o666)
-			stat = Syscall(c).stat(fd)
-			stat_golden = os.stat(f'{self.PATH}/file')
-			assert stat == stat_golden
-
-			with pytest.raises(OSError):
-				Syscall(c).stat(50000) # not existing fd
+				with pytest.raises(OSError):
+					Syscall(c, d).stat(4869)
+		await asyncio.to_thread(test)
